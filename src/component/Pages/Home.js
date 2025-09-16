f2724ed104bf4357b6ffd90c0b755ae98bbb475e
sandra <sandrasandra09257@gmail.com>
2025-09-16 14:51:14 +0530

    changes updated

--- src/component/Pages/Home.js
+++ src/component/Pages/Home.js
@@ -4,8 +4,8 @@ import Logobar from '../logobar/logobar'
 import Grid from '../Grid/Grid'
 import Latestwork from '../Latestwork/Latestwork'
 import Testimonial from '../Testimonial/Testimonial'
-import Contact from '../Contact/Contact'
-import ContactModal from '../../ContactModal/ContactModal'
+import Contact from '../contact/Contact'
+import ContactModal from '../../contactModal/contactModal'
 function Home() {
   const [modal,setmodal]=useState(false)
   return (
